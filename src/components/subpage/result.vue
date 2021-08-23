2f31846a395b74add3c7f520b5091de9acfa8233
0304190225 <945703764@qq.com>
2021-08-24 02:04:14 +0800

    ok

--- src/components/subpage/result.vue
+++ src/components/subpage/result.vue
@@ -1,6 +1,44 @@
 <template>
-  <div>
-    学校
-    有 人报考
+  <div class="result">
+    <el-card>
+    <el-table :data="tableData"
+              stripe
+              style="width: 100%">
+      <el-table-column prop="date"
+                       label="学校"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="name"
+                       label="人数"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="address"
+                       label="其它">
+      </el-table-column>
+    </el-table>
+    </el-card>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      tableData: [{
+        school: '',
+        peopledata: '',
+        else: ''
+      }]
+    }
+  }
+}
+</script>
+<style scoped>
+.el-card{
+  margin-top: 100px;
+  margin-left:275px;
+  width: 1000px;
+}
+.el-table{
+  font-size: 18px;
+}
+</style>
